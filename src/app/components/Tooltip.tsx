66bd39c7e070cda398d3899b9506a74c6605445c
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-04-12 20:57:23 -0300

    fix: adjust charactersList style to separate from bottom

--- src/app/components/Tooltip.tsx
+++ src/app/components/Tooltip.tsx
@@ -1,11 +1,13 @@
+import { cn } from '@/lib/cn';
+
 type TooltipProps = {
   children: React.ReactNode;
   tip: string;
-};
+} & JSX.IntrinsicElements['div'];
 
-export function Tooltip({ children, tip }: TooltipProps) {
+export function Tooltip({ children, tip, className }: TooltipProps) {
   return (
-    <div className="group relative">
+    <div className={cn('group relative', className)}>
       {children}
       <p className="absolute -top-6 left-0 right-0 hidden animate-show-content-up rounded-md p-2 text-center text-xs group-hover:block">
         {tip}
