0d5be9aff30989312f54889b2d06d7d7f357bc71
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-04-14 15:32:47 -0300

    feat: create close modal icon component

--- src/app/@modal/(.)series-list/page.tsx
+++ src/app/@modal/(.)series-list/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { CloseModalIcon } from '@/components/CloseModalIcon';
 import Modal from '@/components/Modal';
 import { createSerieDatasource } from '@/data/serie';
 
@@ -11,7 +12,7 @@ export default async function SeriesListModal() {
     <Modal>
       <div className="relative flex w-64 max-w-md flex-col space-y-3 rounded-md bg-slate-50 px-4 py-2 text-center">
         <Link href="/" className="absolute right-4 text-slate-800">
-          ❌{/* TODO - create `close button component ( use router.back() )` */}
+          <CloseModalIcon />
         </Link>
 
         {series.map((serie) => (
