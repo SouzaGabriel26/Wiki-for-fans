5360fa3ad94c300b130e0d1b7a416178089edf2a
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-04-16 22:36:45 -0300

    feat: create `CallToActions` component

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,20 @@ const config = {
             opacity: 1,
           },
         },
+        'show-content-left': {
+          '0%': {
+            transform: 'translateX(50%)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: 1,
+          },
+        },
       },
       animation: {
         'show-content-up': 'show-content-up  0.2s ease-in-out',
+        'show-content-left': 'show-content-left 0.2s ease-in-out',
       },
     },
   },
