d6b23d66c650a3ce6ef0097e72ee49aeabcb53bd
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-04-19 00:13:29 -0300

    feat: create textarea component

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/lib/cn';
+
+type TextAreaProps = {
+  error?: string;
+} & JSX.IntrinsicElements['textarea'];
+
+export default function TextArea({
+  id,
+  name,
+  placeholder,
+  className,
+  error,
+  ...props
+}: TextAreaProps) {
+  const cols = props.cols ?? 30;
+  const rows = props.rows ?? 5;
+
+  return (
+    <div className="relative">
+      <textarea
+        name={name}
+        id={name}
+        cols={cols}
+        rows={rows}
+        placeholder=" "
+        className={cn(
+          'peer rounded-md border border-slate-500 bg-white px-2 pt-6 text-sm text-slate-700 outline-none placeholder-shown:pt-2 focus:border-slate-800',
+          className,
+        )}
+      />
+      <label
+        htmlFor={id}
+        className="absolute left-2 top-1.5 text-xs text-slate-400 transition-all peer-placeholder-shown:text-base"
+      >
+        {placeholder}
+      </label>
+
+      {error && <small className="pl-2 text-xs text-red-800">{error}</small>}
+    </div>
+  );
+}
