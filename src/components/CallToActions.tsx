ab29af67962db3d70941e4e04ecb4b7847bb2e7d
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-04-19 19:45:23 -0300

    style: fix `Tooltip` animation

--- src/components/CallToActions.tsx
+++ src/components/CallToActions.tsx
@@ -9,14 +9,17 @@ export default function CallToActions() {
   return (
     <div className="fixed bottom-2 right-3 flex flex-col items-center justify-center gap-2 md:absolute md:right-5">
       {actions.map((action) => (
-        <Link key={action.href} href={action.href}>
-          <Tooltip
-            tip={action.tooltip}
-            className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-slate-500 p-2 text-center text-2xl transition-colors hover:bg-slate-600"
-            content_classes="top-1/2 -translate-y-1/2 font-bold text-start w-fit -left-28"
-          >
+        <Link
+          key={action.href}
+          href={action.href}
+          className="max-w group relative flex items-center justify-center"
+        >
+          <p className="absolute -left-[130px] w-[120px] text-end text-sm font-bold opacity-0 transition-all ease-in-out group-hover:opacity-100">
+            {action.name}
+          </p>
+          <div className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-slate-500 p-2 text-center text-2xl transition-colors hover:bg-slate-600">
             {action.content}
-          </Tooltip>
+          </div>
         </Link>
       ))}
     </div>
