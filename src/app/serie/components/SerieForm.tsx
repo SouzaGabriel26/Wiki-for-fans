96a881f15235fe555ae51db62e4421d259b5a843
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-04-29 18:15:08 -0300

    refactor: adjust `SerieForm` component to use in both edit and crete page

---
+++ src/app/serie/components/SerieForm.tsx
@@ -0,0 +1,85 @@
+import { CreateSerieArgs } from '@/app/api/configs/context/prismaMutations';
+import Input from '@/components/Input';
+import InputMultiSelect from '@/components/InputMultiSelect';
+import { SubmitButton } from '@/components/SubmitButton';
+import TextArea from '@/components/TextArea';
+
+type SerieFormProps = {
+  isEdit?: boolean;
+  serie?: CreateSerieArgs;
+  action: (formData: FormData) => Promise<void>;
+};
+
+export default async function SerieForm({
+  isEdit = false,
+  serie,
+  action,
+}: SerieFormProps) {
+  if (!isEdit && serie) {
+    throw new Error(
+      '> SerieForm: To use this component in edit mode, you must provide a character.',
+    );
+  }
+
+  return (
+    <form key={new Date().toISOString()} className="space-y-4" action={action}>
+      <Input
+        id="name"
+        name="name"
+        placeholder="Name"
+        required
+        defaultValue={serie?.name}
+      />
+      <TextArea
+        required
+        id="description"
+        name="description"
+        placeholder="Description"
+        defaultValue={serie?.description}
+      />
+      <div className="flex gap-2">
+        <Input
+          required
+          id="episodes"
+          type="number"
+          name="episodes"
+          placeholder="Episodes"
+          defaultValue={serie?.episodes}
+        />
+        <Input
+          required
+          id="seasons"
+          type="number"
+          name="seasons"
+          placeholder="Seasons"
+          defaultValue={serie?.seasons}
+        />
+      </div>
+      <InputMultiSelect
+        id="platforms"
+        name="platforms"
+        placeholder="Available platforms"
+        defaultOptions={serie?.platforms}
+      />
+      <fieldset className="space-x-2">
+        <label htmlFor="status">Status</label>
+        <select
+          required
+          id="status"
+          name="status"
+          className="rounded-md outline-none"
+          defaultValue={serie?.status}
+        >
+          <option value=""></option>
+          <option value="FINISHED">FINISHED</option>
+          <option value="CANCELED">CANCELED</option>
+          <option value="IN_PROGRESS">IN PROGRESS</option>
+        </select>
+      </fieldset>
+
+      <SubmitButton className="bottom-5 right-10 py-2 md:absolute">
+        {isEdit ? 'Save' : 'Register'}
+      </SubmitButton>
+    </form>
+  );
+}
--- src/app/serie/create/page.tsx
+++ src/app/serie/create/page.tsx
@@ -1,8 +1,70 @@
+import { revalidatePath } from 'next/cache';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
 import { NavigateBack } from '@/components/NavigateBack';
+import { createSerieDatasource } from '@/data/serie';
 
-import SerieForm from './components/SerieForm';
+import SerieForm from '../components/SerieForm';
+
+const serieSchema = z.object({
+  name: z.string({
+    required_error: 'Name is required',
+  }),
+  description: z
+    .string({
+      required_error: 'Description is required',
+    })
+    .min(10, 'Description must have at least 10 characters'),
+  episodes: z.number({
+    required_error: 'Episodes is required',
+  }),
+  seasons: z.number({
+    required_error: 'Seasons is required',
+  }),
+  platforms: z.array(z.string()),
+  status: z.enum(['FINISHED', 'CANCELED', 'IN_PROGRESS'], {
+    required_error: 'Status is required',
+  }),
+});
+
+async function serverActionToCreateSerie(formData: FormData) {
+  'use server';
+
+  const entries = Object.fromEntries(formData.entries());
+
+  const treatedData = {
+    name: entries.name,
+    description: entries.description,
+    episodes: Number(entries.episodes),
+    seasons: Number(entries.seasons),
+    platforms: JSON.parse(entries.platformsArray as string),
+    status: entries.status,
+  };
+
+  const dataAfterValidation = serieSchema.safeParse(treatedData);
+
+  if (!dataAfterValidation.success) {
+    return redirect(
+      `/serie/create?error=${dataAfterValidation.error.errors[0].message}`,
+    );
+  }
+
+  const serieDataSource = createSerieDatasource();
+  const { createdSerie, errors } = await serieDataSource.create(
+    dataAfterValidation.data,
+  );
+
+  if (!createdSerie) {
+    return redirect(`/serie/create?error=${errors.message}`);
+  }
+
+  if (!errors) {
+    revalidatePath('/');
+    return redirect('/');
+  }
+}
 
 type Props = {
   searchParams: {
@@ -41,7 +103,7 @@ export default function Page({ searchParams }: Props) {
         </div>
       )}
       <div className="mx-auto mt-10 max-w-72 flex-grow overflow-y-auto px-4 py-2 md:max-w-md">
-        <SerieForm />
+        <SerieForm action={serverActionToCreateSerie} />
       </div>
     </div>
   );
