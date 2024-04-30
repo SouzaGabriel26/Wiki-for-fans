5e949ea37a6718ae6c7ec8a1bc771caeb64841a1
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2024-04-30 20:03:44 -0300

    refactor: adjustments in `serie/[serieId]` page

--- src/app/serie/[serieId]/page.tsx
+++ src/app/serie/[serieId]/page.tsx
@@ -24,7 +24,7 @@ export default async function Page({ params }: Params) {
         : 'border-yellow-500 bg-yellow-100';
 
   return (
-    <div className="relative space-y-2 p-4 text-center">
+    <div className="relative flex h-full flex-col space-y-2 p-4 text-center">
       <NavigateBack className="absolute left-1 top-1 hover:underline md:left-5 md:top-4" />
 
       <div className="absolute right-1 top-0 mt-0 flex items-center gap-2">
@@ -38,39 +38,43 @@ export default async function Page({ params }: Params) {
 
       <h1 className="mb-4 text-2xl">{returnedSerie.name}</h1>
 
-      <div>
-        <strong>Description</strong>
-        <p>{returnedSerie.description}</p>
-      </div>
+      <div className="flex h-full flex-grow flex-col items-center justify-center gap-4">
+        <div className="max-h-[300px] max-w-[500px] overflow-y-auto">
+          <div className="sticky top-0 w-full bg-white">
+            <strong>Description</strong>
+          </div>
+          <p>{returnedSerie.description}</p>
+        </div>
 
-      <div>
-        <strong>Episodes</strong>
-        <p>{returnedSerie.episodes}</p>
-      </div>
+        <div>
+          <strong>Episodes</strong>
+          <p>{returnedSerie.episodes}</p>
+        </div>
 
-      <div>
-        <strong>Seasons</strong>
-        <p>{returnedSerie.seasons}</p>
-      </div>
+        <div>
+          <strong>Seasons</strong>
+          <p>{returnedSerie.seasons}</p>
+        </div>
 
-      <div>
-        <strong>Platforms</strong>
-        <p>{returnedSerie.platforms}</p>
-      </div>
+        <div>
+          <strong>Platforms</strong>
+          <p>{returnedSerie.platforms}</p>
+        </div>
 
-      <div>
-        <strong>Status</strong>
-        <p className={cn(`mx-auto w-fit rounded border px-2`, statusColor)}>
-          {returnedSerie.status}
-        </p>
-      </div>
+        <div>
+          <strong>Status</strong>
+          <p className={cn(`mx-auto w-fit rounded border px-2`, statusColor)}>
+            {returnedSerie.status}
+          </p>
+        </div>
 
-      <div>
-        <strong>Number of Characters registered</strong>
-        <p>{returnedSerie.characters.length}</p>
-        <Link href={`/?serieId=${params.serieId}`} className="text-xs">
-          See characters page
-        </Link>
+        <div>
+          <strong>Number of Characters registered</strong>
+          <p>{returnedSerie.characters.length}</p>
+          <Link href={`/?serieId=${params.serieId}`} className="text-xs">
+            See characters page
+          </Link>
+        </div>
       </div>
     </div>
   );
