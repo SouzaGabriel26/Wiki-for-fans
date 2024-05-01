13e57c9d5a78b6d63d6dd0d942a576f762532995
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-05-01 19:07:04 -0300

    chore: apollo client config

--- src/lib/clientApi.ts
+++ src/lib/clientApi.ts
@@ -1,9 +1,16 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
 const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/graphql';
+  process.env.NODE_ENV == 'production'
+    ? '/api/graphql'
+    : 'http://localhost:3000/api/graphql';
+
+const httpLink = createHttpLink({
+  uri: API_URL,
+});
 
 export const client = new ApolloClient({
   uri: API_URL,
   cache: new InMemoryCache(),
+  link: httpLink,
 });
--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prisma: undefined | PrismaClient;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV === 'production') globalThis.prismaGlobal = prisma;
