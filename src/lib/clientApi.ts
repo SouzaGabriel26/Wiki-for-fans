4c3aea2b201e3e9352df0baa7f95806015f96e55
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-05-01 17:20:13 -0300

    chore: adding public url to apollo config

--- src/lib/clientApi.ts
+++ src/lib/clientApi.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const API_URL = 'http://localhost:3000/api/graphql';
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/graphql';
 
 export const client = new ApolloClient({
   uri: API_URL,
