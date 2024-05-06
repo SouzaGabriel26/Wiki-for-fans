c490dcb8c42ecfdf08ebd22cf9dcf16fd7faa15c
Gabriel Souza <gabriel.asouza2608@hotmail.com>
2024-05-06 17:58:55 -0300

    feat: enable vercel analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './main.css';
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en" className="h-screen">
       <body className={inter.className} style={{ height: '100vh' }}>
         {children}
+        <Analytics />
         {modal}
       </body>
     </html>
